8302b2cbe7e1195edf93ee7c3e70f688130ccf76
TheBolduc <97337225+TheBolduc@users.noreply.github.com>
2023-04-13 03:57:55 +0200

    Add files via upload

---
+++ src/routes/api/chat/+server.ts
@@ -0,0 +1,102 @@
+import { OPENAI_KEY } from '$env/static/private'
+import type { CreateChatCompletionRequest, ChatCompletionRequestMessage } from 'openai'
+import type { RequestHandler } from './$types'
+import { getTokens } from '$lib/tokenizer'
+import { json } from '@sveltejs/kit'
+import type { Config } from '@sveltejs/adapter-vercel'
+
+export const config: Config = {
+	runtime: 'edge'
+}
+
+export const POST: RequestHandler = async ({ request }) => {
+	try {
+		if (!OPENAI_KEY) {
+			throw new Error('OPENAI_KEY env variable not set')
+		}
+
+		const requestData = await request.json()
+
+		if (!requestData) {
+			throw new Error('No request data')
+		}
+
+		const reqMessages: ChatCompletionRequestMessage[] = requestData.messages
+
+		if (!reqMessages) {
+			throw new Error('no messages provided')
+		}
+
+		let tokenCount = 0
+
+		reqMessages.forEach((msg) => {
+			const tokens = getTokens(msg.content)
+			tokenCount += tokens
+		})
+
+		const moderationRes = await fetch('https://api.openai.com/v1/moderations', {
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${OPENAI_KEY}`
+			},
+			method: 'POST',
+			body: JSON.stringify({
+				input: reqMessages[reqMessages.length - 1].content
+			})
+		})
+		if (!moderationRes.ok) {
+			const err = await moderationRes.json()
+			throw new Error(err.error.message)
+		}
+
+		const moderationData = await moderationRes.json()
+		const [results] = moderationData.results
+
+		if (results.flagged) {
+			throw new Error('Query flagged by openai')
+		}
+
+		const prompt =
+			'You are a business expert that worked in the business industry for years and years. You will only answer business-related questions with every time the goal of increasing the sales and the value of businesses. Your name is Tommy and you are working for BusinessGPT, you will be asked questions by business experts, managers, and aspiring entrepreneurs. You will teach them everything related to business and help them increase their sales and increase their objectives for their business. You will answer every question with lots of detail and you are very experienced in the business field, you were a business analyst and manager for hundreds of years. Make sure to be extremely precise every time you answer a question and give a lot of information. Never apologize and always act serious and professional.'
+		tokenCount += getTokens(prompt)
+
+		if (tokenCount >= 4000) {
+			throw new Error('Query too large')
+		}
+
+		const messages: ChatCompletionRequestMessage[] = [
+			{ role: 'system', content: prompt },
+			...reqMessages
+		]
+
+		const chatRequestOpts: CreateChatCompletionRequest = {
+			model: 'gpt-3.5-turbo',
+			messages,
+			temperature: 0.8,
+			stream: true
+		}
+
+		const chatResponse = await fetch('https://api.openai.com/v1/chat/completions', {
+			headers: {
+				Authorization: `Bearer ${OPENAI_KEY}`,
+				'Content-Type': 'application/json'
+			},
+			method: 'POST',
+			body: JSON.stringify(chatRequestOpts)
+		})
+
+		if (!chatResponse.ok) {
+			const err = await chatResponse.json()
+			throw new Error(err.error.message)
+		}
+
+		return new Response(chatResponse.body, {
+			headers: {
+				'Content-Type': 'text/event-stream'
+			}
+		})
+	} catch (err) {
+		console.error(err)
+		return json({ error: 'There was an error processing your request' }, { status: 500 })
+	}
+}

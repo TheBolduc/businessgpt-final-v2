dd14878295ac319e8330937a85ada3b1c7a21cad
TheBolduc <97337225+TheBolduc@users.noreply.github.com>
2023-04-16 20:18:26 +0200

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -89,6 +89,6 @@
   <form
     class="flex w-full rounded-md bg-gray-200 py-4 px-2 shadow-inner space-x-2"
     on:submit|preventDefault={() => handleSubmit()}
-	  <input type="text" class="input input-bordered w-full shadow bg-white" bind:value={query} />
-          <button type="submit" class="btn btn-primary bg-green-500 hover:bg-green-600 border border-green-500"> Send </button>
+	<input type="text" class="input input-bordered w-full shadow bg-white" bind:value={query} />
+        <button type="submit" class="btn btn-primary bg-green-500 hover:bg-green-600 border border-green-500"> Send </button>
   </form>

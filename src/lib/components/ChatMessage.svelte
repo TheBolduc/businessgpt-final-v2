48b9a9e36a75001eb2ba1ea96c16e6b25c6a680c
TheBolduc <97337225+TheBolduc@users.noreply.github.com>
2023-04-16 18:59:03 +0200

    Update ChatMessage.svelte

--- src/lib/components/ChatMessage.svelte
+++ src/lib/components/ChatMessage.svelte
@@ -1,21 +1,26 @@
 <script lang="ts">
-	import type { ChatCompletionRequestMessageRoleEnum } from 'openai'
-	export let type: ChatCompletionRequestMessageRoleEnum
-	export let message: string
+  import type { ChatCompletionRequestMessageRoleEnum } from 'openai'
+  export let type: ChatCompletionRequestMessageRoleEnum
+  export let message: string
 </script>
-
 <div class="flex {type === 'user' ? 'justify-end' : 'justify-start'} py-2">
-	<div class="hidden md:block w-10">
-		{#if type === 'assistant'}
-			<img src="https://ui-avatars.com/api/?name=Tommy" alt="Tommy avatar" class="rounded-full w-10 h-10" />
-		{/if}
-	</div>
-	<div class="px-2">
-		{#if type === 'assistant'}
-			<div class="text-sm text-gray-500">Tommy</div>
-		{/if}
-		<div class="bg-gray-250 text-gray-900 p-2 rounded-lg {type === 'user' ? 'bg-blue-500 text-black' : ''}">
-			{message}
-		</div>
-	</div>
+  <div class="hidden md:block w-10">
+    {#if type === 'assistant'}
+      <img src="https://ui-avatars.com/api/?name=Tommy" alt="Tommy avatar" class="rounded-full w-10 h-10" />
+    {/if}
+    {#if type === 'user'}
+      <img src="https://ui-avatars.com/api/?name=User" alt="User avatar" class="rounded-full w-10 h-10" />
+    {/if}
+  </div>
+  <div class="px-2">
+    {#if type === 'assistant'}
+      <div class="text-sm text-gray-500">Tommy</div>
+    {/if}
+    {#if type === 'user'}
+      <div class="text-sm text-gray-500">User</div>
+    {/if}
+    <div class="bg-gray-250 text-gray-900 p-2 rounded-lg {type === 'user' ? 'bg-gray-100 text-black' : ''}">
+      {message}
+    </div>
+  </div>
 </div>

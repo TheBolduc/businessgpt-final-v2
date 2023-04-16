02b08783866fb53c11e81eb6a8d215b6888d3b9f
TheBolduc <97337225+TheBolduc@users.noreply.github.com>
2023-04-16 23:30:23 +0200

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,57 +1,60 @@
 <script lang="ts">
-	import ChatMessage from '$lib/components/ChatMessage.svelte'
-	import type { ChatCompletionRequestMessage } from 'openai'
-	import { SSE } from 'sse.js'
-	let query: string = ''
-	let answer: string = ''
-	let loading: boolean = false
-	let chatMessages: ChatCompletionRequestMessage[] = []
-	let chatContainer: HTMLDivElement
-	function scrollToBottom() {
-		setTimeout(function () {
-			chatContainer.scrollTop = chatContainer.scrollHeight
-		}, 100)
-	}
-	const handleSubmit = async () => {
-		if (!query.trim()) return;
-		loading = true
-		chatMessages = [...chatMessages, { role: 'user', content: query }]
-		const eventSource = new SSE('/api/chat', {
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			payload: JSON.stringify({ messages: chatMessages })
-		})
-		query = ''
-		eventSource.addEventListener('error', handleError)
-		eventSource.addEventListener('message', (e) => {
-			scrollToBottom()
-			try {
-				loading = false
-				if (e.data === '[DONE]') {
-					chatMessages = [...chatMessages, { role: 'assistant', content: answer }]
-					answer = ''
-				        eventSource.close(); // Close the connection
-					return
-				}
-				const completionResponse = JSON.parse(e.data);
-				const [{ delta }] = completionResponse.choices;
-				if (delta.content) {
-					answer = (answer ?? '') + delta.content;
-				}
-			} catch (err) {
-				handleError(err);
-			}
-		})
-		eventSource.stream()
-		scrollToBottom()
-	}
-	function handleError<T>(err: T) {
-		loading = false
-		query = ''
-		answer = ''
-		console.error(err)
-	}
+  import ChatMessage from '$lib/components/ChatMessage.svelte'
+  import type { ChatCompletionRequestMessage } from 'openai'
+  import { SSE } from 'sse.js'
+  let query: string = ''
+  let answer: string = ''
+  let loading: boolean = false
+  let chatMessages: ChatCompletionRequestMessage[] = []
+  let chatContainer: HTMLDivElement
+
+  function scrollToBottom() {
+    setTimeout(function () {
+      chatContainer.scrollTop = chatContainer.scrollHeight
+    }, 100)
+  }
+
+  const handleSubmit = async () => {
+    if (!query.trim()) return
+    loading = true
+    chatMessages = [...chatMessages, { role: 'user', content: query }]
+    const eventSource = new SSE('/api/chat', {
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      payload: JSON.stringify({ messages: chatMessages })
+    })
+    query = ''
+    eventSource.addEventListener('error', handleError)
+    eventSource.addEventListener('message', (e) => {
+      scrollToBottom()
+      try {
+        loading = false
+        if (e.data === '[DONE]') {
+          chatMessages = [...chatMessages, { role: 'assistant', content: answer }]
+          answer = ''
+          eventSource.close() // Close the connection
+          return
+        }
+        const completionResponse = JSON.parse(e.data)
+        const [{ delta }] = completionResponse.choices
+        if (delta.content) {
+          answer = (answer ?? '') + delta.content
+        }
+      } catch (err) {
+        handleError(err)
+      }
+    })
+    eventSource.stream()
+    scrollToBottom()
+  }
+
+  function handleError<T>(err: T) {
+    loading = false
+    query = ''
+    answer = ''
+    console.error(err)
+  }
 </script>
 
 <div class="flex flex-col h-screen pt-4 w-full px-8 items-center gap-2 bg-white">
@@ -61,8 +64,14 @@
     <div class="w-full border-b border-gray-400 mb-4"></div>
   </div>
   <div class="flex flex-col pt-4 w-full h-full px-8 items-center gap-2">
-    <div class="flex flex-col gap-2 max-h-[calc(100vh-250px)] overflow-y-auto" bind:this={chatContainer}>
-      <ChatMessage type="assistant" message="Hi! My name is Tommy, ask me any business-related questions, I'm here to help you grow your business, learn about business, start your business and much more!" />
+    <div
+      class="flex flex-col gap-2 max-h-[calc(100vh-250px)] overflow-y-auto"
+      bind:this={chatContainer}
+    >
+      <ChatMessage
+        type="assistant"
+        message="Hi! My name is Tommy, ask me any business-related questions, I'm here to help you grow your business, learn about business, start your business and much more!"
+      />
       {#each chatMessages as message}
         <ChatMessage type={message.role} message={message.content} />
       {/each}

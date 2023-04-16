9e17ce7da7ca047bb218c203035c007d514bc171
TheBolduc <97337225+TheBolduc@users.noreply.github.com>
2023-04-16 19:33:27 +0200

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -65,32 +65,31 @@
 </script>
 
 <div class="flex flex-col h-screen pt-4 w-full px-8 items-center gap-2 bg-white">
-	<div class="flex-1 flex flex-col items-center justify-center">
-		<h1 class="text-2xl font-bold w-full text-center">BusinessGPT</h1>
-		<div class="w-full border-b border-gray-300 mb-4"></div>
-		<p class="text-sm italic">Your own personal business analyst and assistant.</p>
-		<div class="w-full border-b border-gray-300 mb-4"></div>
-	</div>
-	<div class="flex flex-col pt-4 w-full h-full px-8 items-center gap-2">
-		<div class="flex flex-col gap-2">
-			<ChatMessage type="assistant" message="Hi! My name is Tommy, ask me any business-related questions, I'm here to help you grow your business, learn about business, start your business and much more!" />
-			{#each chatMessages as message}
-				<ChatMessage type={message.role} message={message.content} />
-			{/each}
-			{#if answer}
-				<ChatMessage type="assistant" message={answer} />
-			{/if}
-			{#if loading}
-				<ChatMessage type="assistant" message="Loading.." />
-			{/if}
-		</div>
-		<div class="" bind:this={scrollToDiv} />
-	</div>
-	<form
-		class="flex w-full rounded-md bg-gray-200 p-4 shadow-inner space-x-2"
-		on:submit|preventDefault={() => handleSubmit()}
-	>
-		<input type="text" class="input input-bordered w-full shadow bg-white" bind:value={query} />
-		<button type="submit" class="btn btn-primary bg-green-500 hover:bg-green-600 hover:border-transparent"> Send </button>
-	</form>
+  <div class="flex-1 flex flex-col items-center justify-center">
+    <h1 class="text-2xl font-bold w-full text-center">BusinessGPT</h1>
+    <p class="text-sm italic">Your own personal business analyst and assistant.</p>
+    <div class="w-full border-b border-gray-500 mb-4"></div>
+  </div>
+  <div class="flex flex-col pt-4 w-full h-full px-8 items-center gap-2">
+    <div class="flex flex-col gap-2 max-h-[calc(100vh-250px)] overflow-y-auto">
+      <ChatMessage type="assistant" message="Hi! My name is Tommy, ask me any business-related questions, I'm here to help you grow your business, learn about business, start your business and much more!" />
+      {#each chatMessages as message}
+        <ChatMessage type={message.role} message={message.content} />
+      {/each}
+      {#if answer}
+        <ChatMessage type="assistant" message={answer} />
+      {/if}
+      {#if loading}
+        <ChatMessage type="assistant" message="Loading.." />
+      {/if}
+    </div>
+    <div class="" bind:this={scrollToDiv} />
+  </div>
+  <form
+    class="flex w-full rounded-md bg-gray-200 py-4 px-2 shadow-inner space-x-2"
+    on:submit|preventDefault={() => handleSubmit()}
+  >
+    <input type="text" class="input input-bordered w-full shadow bg-white" bind:value={query} />
+    <button type="submit" class="btn btn-primary bg-green-500 hover:bg-green-600 hover:border-transparent border border-white"> Send </button>
+  </form>
 </div>

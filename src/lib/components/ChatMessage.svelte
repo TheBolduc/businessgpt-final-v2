15318a7f2b5a0b55aef4bb28c1dc0c207e9e9da0
TheBolduc <97337225+TheBolduc@users.noreply.github.com>
2023-04-16 03:00:41 +0200

    Update ChatMessage.svelte

--- src/lib/components/ChatMessage.svelte
+++ src/lib/components/ChatMessage.svelte
@@ -4,19 +4,18 @@
 	export let message: string
 </script>
 
-<div class="chat {type === 'user' ? 'chat-end' : 'chat-start'} justify-end">
-	<div class="chat-image avatar">
-		<div class="w-10 rounded-full">
-			<img
-				src="https://ui-avatars.com/api/?name={type === 'user' ? 'Me' : 'B'}"
-				alt="{type} avatar"
-			/>
+<div class="flex {type === 'user' ? 'justify-end' : 'justify-start'} py-2">
+	<div class="hidden md:block w-10">
+		{#if type === 'assistant'}
+			<img src="https://ui-avatars.com/api/?name=Tommy" alt="Tommy avatar" class="rounded-full" />
+		{/if}
+	</div>
+	<div class="px-2">
+		{#if type === 'assistant'}
+			<div class="text-sm text-gray-500">Tommy</div>
+		{/if}
+		<div class="bg-gray-200 text-gray-800 p-2 rounded-lg {type === 'user' ? 'bg-blue-500 text-white' : ''}">
+			{message}
 		</div>
-	</div>
-	<div class="chat-header">
-		{type === 'user' ? 'Me' : 'Bot'}
-	</div>
-	<div class="chat-bubble {type === 'user' ? 'chat-bubble-primary' : 'chat-bubble-secondary'}">
-		{message}
 	</div>
 </div>

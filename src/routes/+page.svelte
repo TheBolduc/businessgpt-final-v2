29f8f4b83afddf2b982666bef385ebf42e30d845
TheBolduc <97337225+TheBolduc@users.noreply.github.com>
2023-04-16 20:54:03 +0200

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -31,62 +31,6 @@
 				if (e.data === '[DONE]') {
 					chatMessages = [...chatMessages, { role: 'assistant', content: answer }]
 					answer = ''
-					eventSource.close(); // Close the connection
-					return
-				}
-				const completionResponse = JSON.parse(e.data);
-				const [{ delta }] = completionResponse.choices;
-				if (delta.content) {
-					answer = (answer ?? '') + delta.content;
-					scrollToBottom(); // Add this line to scroll as the text is being written
-				}
-			} catch (err) {
-				handleError(err);
-			}
-		})
-		eventSource.stream()
-		scrollToBottom()
-	}
-	function handleError<T>(err: T) {
-		loading = false
-		query = ''
-		answer = ''
-		console.error(err)
-	}
-
-<script lang="ts">
-	import ChatMessage from '$lib/components/ChatMessage.svelte'
-	import type { ChatCompletionRequestMessage } from 'openai'
-	import { SSE } from 'sse.js'
-	let query: string = ''
-	let answer: string = ''
-	let loading: boolean = false
-	let chatMessages: ChatCompletionRequestMessage[] = []
-	let scrollToDiv: HTMLDivElement
-	function scrollToBottom() {
-		setTimeout(function () {
-			scrollToDiv.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'nearest' })
-		}, 100)
-	}
-	const handleSubmit = async () => {
-		if (!query.trim()) return;
-		loading = true
-		chatMessages = [...chatMessages, { role: 'user', content: query }]
-		const eventSource = new SSE('/api/chat', {
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			payload: JSON.stringify({ messages: chatMessages })
-		})
-		query = ''
-		eventSource.addEventListener('error', handleError)
-		eventSource.addEventListener('message', (e) => {
-			scrollToBottom()
-			try {
-				loading = false
-				if (e.data === '[DONE]') {
-					chatMessages = [...chatMessages, { role: 'assistant', content: answer }]
-					answer = ''
 				        eventSource.close(); // Close the connection
 					return
 				}

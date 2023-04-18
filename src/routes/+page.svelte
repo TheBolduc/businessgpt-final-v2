c0bbeb7c63ef9fa15b37fc228dab60f1e5c3e561
TheBolduc <97337225+TheBolduc@users.noreply.github.com>
2023-04-18 19:28:36 +0200

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,50 +1,73 @@
 <script lang="ts">
   import ChatMessage from '$lib/components/ChatMessage.svelte';
-  import { onMount } from "svelte";
+  import type { ChatCompletionRequestMessage } from 'openai';
+  import { SSE } from 'sse.js';
 
   let query: string = '';
   let answer: string = '';
   let loading: boolean = false;
-  let chatMessages: { role: string, content: string }[] = [];
+  let chatMessages: ChatCompletionRequestMessage[] = [];
 
+  let scrollToDiv: HTMLDivElement;
   let inputField: HTMLInputElement;
+  let chatContainer: HTMLDivElement;
 
-  async function askQuestion() {
-    if (query.trim() === "") return;
-    loading = true;
-    try {
-      // Replace this with the actual function to get the answer
-      const response = await getAnswer(query);
-      answer = response;
-    } catch (error) {
-      console.error(error);
-      answer = "An error occurred. Please try again.";
-    } finally {
-      chatMessages = [...chatMessages, { role: 'user', content: query }];
-      query = '';
-      loading = false;
+  $: {
+    if (scrollToDiv) {
+      scrollToDiv.scrollIntoView({ behavior: 'smooth' });
     }
   }
 
-  async function handleSubmit(event) {
-    event.preventDefault();
-    await askQuestion();
-    chatMessages = [...chatMessages, { role: 'assistant', content: answer }];
-    answer = '';
-    inputField.focus();
+  async function handleSubmit() {
+    if (loading) {
+      return;
+    }
+
+    if (!query.trim()) {
+      return;
+    }
+
+    loading = true;
+    chatMessages = [...chatMessages, { role: 'user', content: query }];
+    const eventSource = new SSE('/api/chat', {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      payload: JSON.stringify({ messages: chatMessages }),
+    });
+    query = '';
+    inputField.disabled = true;
+    eventSource.addEventListener('error', handleError);
+    eventSource.addEventListener('message', (e) => {
+      try {
+        if (e.data === '[DONE]') {
+          chatMessages = [...chatMessages, { role: 'assistant', content: answer }];
+          answer = '';
+          eventSource.close(); // Close the connection
+          loading = false;
+          inputField.disabled = false;
+          scrollToDiv.scrollIntoView({ behavior: 'smooth' });
+          return;
+        }
+        const completionResponse = JSON.parse(e.data);
+        const [{ text }] = completionResponse.choices; // Changed from delta to text
+        if (text) {
+          answer = (answer ?? '') + text; // Changed from delta.content to text
+          chatContainer.scrollTop = chatContainer.scrollHeight;
+        }
+      } catch (err) {
+        handleError(err);
+      }
+    });
+    eventSource.stream();
   }
 
-  onMount(() => {
-    inputField.focus();
-  });
-
-  // Dummy getAnswer function, replace it with the actual API call or function
-  async function getAnswer(query) {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        resolve(`You asked: ${query}`);
-      }, 1000);
-    });
+  function handleError<T>(err: T) {
+    loading = false;
+    query = '';
+    answer = '';
+    inputField.disabled = false;
+    console.error(err);
   }
 </script>
 
@@ -65,8 +88,13 @@
         <div class="w-full border-b border-gray-400 mb-6"></div>
       </div>
       <div class="flex flex-col pt-4 w-full h-full px-8 items-center gap-2">
-        <div class="flex flex-col gap-2 h-[calc(100vh-300px)] overflow-y-auto mt-4 mb-4">
+        <div class="flex flex-col gap-2 h-[calc(100vh-300px)] overflow-y-auto mt-4 mb-4" bind:this={chatContainer}>
+          <ChatMessage
+            type="assistant"
+            message="Hi! My name is Tommy, ask me any business-related questions, I'm here to help you grow your business, learn about business, start your business and much more!"
+          />
           {#each chatMessages as message}
+
             <ChatMessage type={message.role} message={message.content} />
           {/each}
           {#if answer}
@@ -76,14 +104,23 @@
             <ChatMessage type="assistant" message="Loading.." />
           {/if}
         </div>
+        <div class="" bind:this={scrollToDiv} />
       </div>
-      <form class="flex w-full rounded-md bg-gray-200 py-6 px-2 shadow-inner space-x-2"
-            on:submit|preventDefault={handleSubmit}>
-        <input type="text" class="input input-bordered w-full shadow bg-white text-black"
-               bind:this={inputField} bind:value={query} placeholder="Ask me anything about business..."
-               on:keydown={(event) => event.key === "Enter" && askQuestion()}
-               disabled={loading} />
-        <button type="submit" class="btn btn-primary bg-green-500 hover:bg-green-600 border border-green-500 hover:border-green-600"
+      <form
+        class="flex w-full rounded-md bg-gray-200 py-6 px-2 shadow-inner space-x-2"
+        on:submit|preventDefault={() => handleSubmit()}
+      >
+        <input
+          type="text"
+          class="input input-bordered w-full shadow bg-white text-black"
+          bind:this={inputField}
+          bind:value={query}
+          placeholder="Ask me anything about business..."
+          disabled={loading}
+        />
+        <button
+          type="submit"
+          class="btn btn-primary bg-green-500 hover:bg-green-600 border border-green-500 hover:border-green-600"
           disabled={loading}
         >
           Send

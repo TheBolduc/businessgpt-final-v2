281f901fafc34e05c1bd5235c0e5d8ff3fa755f5
TheBolduc <97337225+TheBolduc@users.noreply.github.com>
2023-04-20 19:55:57 +0200

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,29 +1,24 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
-import { resolve } from 'path';
 
 const config = {
-  preprocess: preprocess(),
-  kit: {
-    adapter: adapter({
-      // default options are shown
-      pages: 'build',
-      assets: 'build',
-      fallback: null
-    }),
-    target: '#svelte',
-    vite: {
-      resolve: {
-        alias: {
-          $components: resolve('./src/components'),
-          $lib: resolve('./src/lib'),
-        }
-      },
-      ssr: {
-        external: ['svelte-routing']
-      }
-    }
-  }
+    kit: {
+        adapter: adapter({
+            // default options are shown
+            pages: 'build',
+            assets: 'build',
+            fallback: null
+        }),
+        target: '#svelte',
+        vite: {
+            plugins: [],
+        },
+        ssr: {
+            noExternal: Object.keys(pkg.dependencies || {}),
+        },
+    },
+
+    preprocess: preprocess(),
 };
 
 export default config;

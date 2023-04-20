18c476acff097827cf622b260ac794ffb0e5e33d
TheBolduc <97337225+TheBolduc@users.noreply.github.com>
2023-04-20 19:36:58 +0200

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,20 @@
 <script>
-	import '../app.css'
+	import { Router, Route } from "svelte-routing";
+	import Index from "./routes/index.svelte";
+	import BusinessOperations from "./routes/business_operations.svelte";
+	import CustomerRelationship from "./routes/customer_relationship.svelte";
+	import DataDrivenDecisionMaking from "./routes/data_driven_decision_making.svelte";
+	import InnovationSustainabilityGrowth from "./routes/innovation_sustainability_growth.svelte";
+	import SalesMarketingCustomerExperience from "./routes/sales_marketing_customer_experience.svelte";
+	import WorkforceManagementDevelopment from "./routes/workforce_management_development.svelte";
 </script>
 
-<div class="flex flex-col items-center max-w-2xl mx-auto">
-	<slot />
-</div>
+<Router>
+	<Route path="/" component="{Index}" />
+	<Route path="/business_operations" component="{BusinessOperations}" />
+	<Route path="/customer_relationship" component="{CustomerRelationship}" />
+	<Route path="/data_driven_decision_making" component="{DataDrivenDecisionMaking}" />
+	<Route path="/innovation_sustainability_growth" component="{InnovationSustainabilityGrowth}" />
+	<Route path="/sales_marketing_customer_experience" component="{SalesMarketingCustomerExperience}" />
+	<Route path="/workforce_management_development" component="{WorkforceManagementDevelopment}" />
+</Router>
